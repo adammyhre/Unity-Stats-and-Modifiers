fdb91a294dbf09509dd2d7d3bf5f881341c87dcf
Adam Myhre <adam@undergalaxie.com>
2024-04-25 04:50:51 -0700

    Remove Factory

--- Assets/_Project/Scripts/Stats/StatsFactory.cs
+++
@@ -1,8 +0,0 @@
-using Malevolent;
-
-public class StatsFactory {
-    public Stats Create(BaseStats baseStats) {
-        Preconditions.CheckNotNull(baseStats, nameof(baseStats));
-        return new Stats(new StatsMediator(), baseStats);
-    }
-}
